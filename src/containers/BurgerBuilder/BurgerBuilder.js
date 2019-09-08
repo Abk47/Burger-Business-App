0552354bf40e11da924e3a264178f1b99ccff2e3
Abuka <maacsgaming@gmail.com>
2019-09-09 02:08:40 +0300

    Add ingredients and their css

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,11 +1,20 @@
 import React, { Component } from 'react'
 import Aux from '../../hoc/auxillary'
+import Burger from '../../components/Burger/Burger'
 
 class BurgerBuilder extends Component {
+  state = {
+    ingredients : {
+      salad : 1,
+      bacon : 1,
+      meat : 2,
+      cheese: 1
+    }
+  }
   render () {
     return (
       <Aux>
-        <div>Burger</div>
+        <Burger ingredients={this.state.ingredients} />
         <div>Build Controls</div>
       </Aux>
     )
