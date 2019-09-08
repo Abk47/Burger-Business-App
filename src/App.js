f7d9add83d015166ba48261387e7de67a978415e
Abuka <maacsgaming@gmail.com>
2019-09-08 22:46:26 +0300

    remove boilerplate

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 
 class App extends Component {
-  render() {
+  render () {
     return (
-      <div className="App">
+      <div>
         Hello
       </div>
-    );
+    )
   }
 }
 
