a88b769a0738b3e42afcb690ba93e630823f2876
Abuka <maacsgaming@gmail.com>
2019-09-11 03:53:57 +0300

    Add modal-backdrop functionality

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,16 +1,21 @@
 import React from 'react'
 import classes from './Modal.css'
+import Aux from '../../../hoc/auxillary'
+import BackDrop from '../BackDrop/BackDrop'
 
 const Modal = (props) => (
-  <div
-    className={classes.Modal}
-    style={{
-      transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
-      opacity: props.show ? '1' : '0'
-    }}
-  >
-    {props.children}
-  </div>
+  <Aux>
+    <BackDrop show={props.show} hide={props.modalClosed} />
+    <div
+      className={classes.Modal}
+      style={{
+        transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
+        opacity: props.show ? '1' : '0'
+      }}
+    >
+      {props.children}
+    </div>
+  </Aux>
 )
 
 export default Modal
