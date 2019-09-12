a5fca6bbad0ef285dd720201490266e886a48881
Abuka <maacsgaming@gmail.com>
2019-09-12 03:27:05 +0300

    Feat: SideDrawer and backDrop toggle

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,17 +1,31 @@
-import React from 'react'
+import React, { Component } from 'react'
 import Aux from '../../hoc/auxillary'
 import classes from './Layout.css'
 import Toolbar from '../Navigation/Toolbar/Toolbar'
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer'
 
-const Layout = (props) => (
-  <Aux>
-    <Toolbar />
-    <SideDrawer />
-    <main className={classes.Content}>
-      {props.children}
-    </main>
-  </Aux>
-)
+class Layout extends Component {
+state = {
+  showSideDrawer: true
+}
+
+  closingSideDrawer = () => {
+    this.setState({ showSideDrawer: false })
+  }
+  
+  render () {
+    return (
+      <Aux>
+        <Toolbar />
+        <SideDrawer 
+          open={this.state.showSideDrawer} 
+          closed={this.closingSideDrawer} />
+        <main className={classes.Content}>
+          {this.props.children}
+        </main>
+      </Aux>
+    )
+  }
+}
 
 export default Layout
