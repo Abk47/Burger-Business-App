9ef3c7fad8950a0ca8ea373a097e0431ce1f41fa
Abuka <maacsgaming@gmail.com>
2019-09-12 04:04:13 +0300

    Add SideDrawer and backDrop toggle functionality

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -6,17 +6,23 @@ import SideDrawer from '../Navigation/SideDrawer/SideDrawer'
 
 class Layout extends Component {
 state = {
-  showSideDrawer: true
+  showSideDrawer: false
 }
 
   closingSideDrawer = () => {
     this.setState({ showSideDrawer: false })
   }
   
+  drawerToggleClickHandler = () => {
+    this.setState((prevState) => {
+      return {showSideDrawer: !prevState.showSideDrawer}
+    })
+  }
+  
   render () {
     return (
       <Aux>
-        <Toolbar />
+        <Toolbar DrawerToggleClicked={this.drawerToggleClickHandler} />
         <SideDrawer 
           open={this.state.showSideDrawer} 
           closed={this.closingSideDrawer} />
