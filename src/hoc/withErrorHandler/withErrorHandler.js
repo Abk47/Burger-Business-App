ede4cc9af800d9cf7c0041ed086dfafd2d774b07
Abuka <maacsgaming@gmail.com>
2019-09-15 13:56:32 +0300

    Change into class component

--- src/hoc/withErrorHandler/withErrorHandler.js
+++ src/hoc/withErrorHandler/withErrorHandler.js
@@ -1,17 +1,19 @@
-import React from 'react'
+import React, { Component } from 'react'
 import Modal from '../../components/UI/Modal/Modal'
 import Aux from '../auxillary'
 
-const withErrorHandler = (WrappedComponent) => {
-  return (props) => {
-    return (
-      <Aux>
-        <Modal>
-            Something didn't work!
-        </Modal>
-        <WrappedComponent {...props} />
-      </Aux>
-    )
+const withErrorHandler = (WrappedComponent, axios) => {
+  return class extends Component {
+    render () {
+      return (
+        <Aux>
+          <Modal>
+            <div style={{ textAlign: 'center' }}>Something didn't work!</div>
+          </Modal>
+          <WrappedComponent {...this.props} />
+        </Aux>
+      )
+    }
   }
 }
 
