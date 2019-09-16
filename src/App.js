c033f9e66c2e7fbd65321bc5f73d3f0d5a9ebb7d
Abuka <maacsgaming@gmail.com>
2019-09-17 01:09:33 +0300

    Add routing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React, { Component } from 'react'
 import Layout from './components/Layout/Layout'
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 
 class App extends Component {
   render () {
     return (
-      <Layout>
-        <BurgerBuilder />
-      </Layout>
+      <BrowserRouter>
+        <Switch>
+          <Layout>
+            <Route path='/' exact component={BurgerBuilder} />
+          </Layout>
+        </Switch>
+      </BrowserRouter>
     )
   }
 }
