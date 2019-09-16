599734e9a9af2576e400e21a32e341287bef8703
Abuka <maacsgaming@gmail.com>
2019-09-17 02:57:48 +0300

    Add form

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import Button from '../../../components/UI/Button/Button'
+import classes from './ContactData.css'
 
 class ContactData extends Component {
     state = {
@@ -12,14 +13,14 @@ class ContactData extends Component {
     }
 render() {
     return (
-         <div>
+         <div className={classes.ContactData}>
              <h4>Enter your contact details here</h4>
              <form>
-                 <input type='text' name='name' placeholder='Full Name' />
-                 <input type='email' name='email' placeholder='Email address' />
-                 <input type='text' name='street' placeholder='Street' />
-                 <input type='text' name='postalCode' placeholder='Postal code' />
-                 <Button btnType='success'>Place ORDER</Button>
+                 <input className={classes.input} type='text' name='name' placeholder='Full Name' />
+                 <input className={classes.input} type='email' name='email' placeholder='Email address' />
+                 <input className={classes.input} type='text' name='street' placeholder='Street' />
+                 <input className={classes.input} type='text' name='postalCode' placeholder='Postal code' />
+                 <Button btnType='Success'>ORDER</Button>
              </form>
          </div>
 
