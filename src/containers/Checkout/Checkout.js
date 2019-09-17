d21ba083b5621a05943cb2386fb9b65927b236cb
Abuka <maacsgaming@gmail.com>
2019-09-17 03:37:32 +0300

    Extract query params methods

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -1,15 +1,27 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+import { Route } from 'react-router-dom'
 
-import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSummary';
+import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSummary'
+import ContactData from './ContactData/ContactData'
 
 class Checkout extends Component {
     state = {
-        ingredients: {
-            salad: 1,
-            meat: 1,
-            cheese: 1,
-            bacon: 1
+        ingredients: null,
+        totalPrice: 0
+    }
+
+    componentWillMount(){
+        const query = new URLSearchParams(this.props.location.search)
+        const ingredients = {}
+        let price = null
+        for( let params of query.entries()){
+            if(params[0] === 'price'){
+                price = params[1]
+            } else{
+                ingredients[params[0]] = +params[1]
+            }
         }
+        this.setState({ ingredients: ingredients, totalPrice: price })
     }
 
     checkoutCancelHandler = () => {
@@ -17,7 +29,7 @@ class Checkout extends Component {
     }
 
     checkoutContinueHandler = () => {
-        console.log('Abk')
+        this.props.history.replace('/checkout/contact')        
     }
 
     render() {
@@ -27,6 +39,8 @@ class Checkout extends Component {
                 ingredients={this.state.ingredients}
                 checkoutCancelled={this.checkoutCancelHandler}
                 checkoutContinued={this.checkoutContinueHandler} />
+                <Route path={this.props.match.path + '/contact'} 
+                render={() => <ContactData ingredients={this.state.ingredients} price={this.state.totalPrice} />} />
             </div>
         );
     }
