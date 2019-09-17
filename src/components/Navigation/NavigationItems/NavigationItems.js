176e5d41bcf3fe78190d523211d8384c7b51303d
Abuka <maacsgaming@gmail.com>
2019-09-17 04:51:35 +0300

    Add NavLink

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -3,11 +3,10 @@ import React from 'react'
 import classes from './NavigationItems.css'
 import NavigationItem from './NavigationItem/NavigationItem'
 
-
 const NavigationItems = (props) => (
   <ul className={classes.NavigationItems}>
-    <NavigationItem link='/' active>Burger Builder</NavigationItem>
-    <NavigationItem link='/'>Checkout page</NavigationItem>
+    <NavigationItem link='/' exact>Burger Builder</NavigationItem>
+    <NavigationItem link='/orders'>Orders</NavigationItem>
   </ul>
 )
 
