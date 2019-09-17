68f4ddc01c867a29972e6b44c2f17e62d5dd124b
Abuka <maacsgaming@gmail.com>
2019-09-17 04:10:16 +0300

    Add spinner

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react'
+import { withRouter } from 'react-router-dom'
+
 import Button from '../../../components/UI/Button/Button'
 import classes from './ContactData.css'
 import axios from '../../../axios-orders'
+import Spinner from '../../../components/UI/Spinner/Spinner'
 
 class ContactData extends Component {
     state = {
@@ -32,26 +35,32 @@ class ContactData extends Component {
         try {
         await axios.post('/orders.json', order)
           this.setState({ loading: false })
+          this.props.history.push('/')
         } catch (error) {
           this.setState({ loading: false })
         }
     }
 
 render() {
+  let form = (
+  <form>
+    <input className={classes.input} type='text' name='name' placeholder='Full Name' />
+    <input className={classes.input} type='email' name='email' placeholder='Email address' />
+    <input className={classes.input} type='text' name='street' placeholder='Street' />
+    <input className={classes.input} type='text' name='postalCode' placeholder='Postal code' />
+    <Button btnType='Success' clicked={this.orderHandler}>ORDER</Button>
+  </form>
+  )
+  if(this.state.loading){
+    form = <Spinner />
+  }
     return (
          <div className={classes.ContactData}>
              <h4>Enter your contact details here</h4>
-             <form>
-                 <input className={classes.input} type='text' name='name' placeholder='Full Name' />
-                 <input className={classes.input} type='email' name='email' placeholder='Email address' />
-                 <input className={classes.input} type='text' name='street' placeholder='Street' />
-                 <input className={classes.input} type='text' name='postalCode' placeholder='Postal code' />
-                 <Button btnType='Success' clicked={this.orderHandler}>ORDER</Button>
-             </form>
+             {form}
          </div>
-
     )
 }
 }
 
-export default ContactData
+export default withRouter(ContactData)
